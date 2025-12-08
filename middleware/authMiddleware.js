6232579cbb062c5f43c8c967136c1387597c84bd
ashsingh138 <ashking1308@gmail.com>
2025-12-09 02:50:06 +0530

    Remove .env from repository

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import FitUser from '../models/User.js';
 
 const protect = async (req, res, next) => {
   let token;
@@ -13,7 +13,7 @@ const protect = async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       // Find user in DB and attach to request
-      req.user = await User.findById(decoded.id).select('-password');
+      req.user = await FitUser.findById(decoded.id).select('-password');
 
       next();
     } catch (error) {
